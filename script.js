d3f553701267686158590e4574f19807e3b32ec8
Crush <72803722+AhmadHnif@users.noreply.github.com>
2024-12-26 18:43:58 +0700

    add getComputerChoice function
    To show computer answer

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello world! from external script.js");
+
+function getComputerChoice(max) { 
+    let x = Math.floor(Math.random() * max);
+
+    if (x == 0) {
+        x = "rock"; 
+    } else if (x == 1) {
+        x = "paper";
+    } else if (x == 2) {
+        x = "scissors";
+    } else {
+        x = "anomaly";
+    }
+
+    return x;
+
+}
+
+let y = getComputerChoice(3);
+
+console.log(y);
